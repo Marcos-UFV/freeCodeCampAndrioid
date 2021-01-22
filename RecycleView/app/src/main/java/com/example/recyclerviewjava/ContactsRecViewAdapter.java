608a0532fc3dcedb41a1d4ea4ecb5f903954c514
Marcos Aurélio <marcospgd10@gmail.com>
2021-01-22 15:11:52 -0300

    Parte 3 RecyclerView

--- RecycleView/app/src/main/java/com/example/recyclerviewjava/ContactsRecViewAdapter.java
+++ RecycleView/app/src/main/java/com/example/recyclerviewjava/ContactsRecViewAdapter.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -32,12 +36,17 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.textName.setText(contacts.get(position).getName());
+        holder.textEmail.setText(contacts.get(position).getEmail());
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context,contacts.get(position).getName()+"Selected",Toast.LENGTH_LONG).show();
             }
         });
+        Glide.with(context)
+                .asBitmap()
+                .load(contacts.get(position).getImageUrl())
+                .into(holder.image);
     }
 
     @Override
@@ -51,12 +60,15 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView textName;
-        RelativeLayout parent;
+        private TextView textName,textEmail;
+        private CardView parent;
+        private ImageView image;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textName = itemView.findViewById(R.id.textName);
             parent = itemView.findViewById(R.id.parent);
+            textEmail = itemView.findViewById(R.id.textEmail);
+            image = itemView.findViewById(R.id.image);
         }
     }
 }
