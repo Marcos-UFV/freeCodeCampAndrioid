737d6233b96a92060c769f115fefacd95f4efa3c
Marcos <aurelio.marcos@ufv.br>
2021-01-23 18:53:25 -0300

    Singleton Request Queue in Volley

--- WeatherAPIApp/app/src/main/java/com/example/weatherapiapp/MainActivity.java
+++ WeatherAPIApp/app/src/main/java/com/example/weatherapiapp/MainActivity.java
@@ -44,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 String url ="https://www.metaweather.com/api/location/search/?query="+et_dataInput.getText().toString();
 
                 JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,url, null,new Response.Listener<JSONArray>() {
@@ -66,26 +64,8 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this,"Something wrong.",Toast.LENGTH_SHORT).show();
                     }
                 });
-                queue.add(request);
-//
-//                // Request a string response from the provided URL.
-//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-//                        new Response.Listener<String>() {
-//                            @Override
-//                            public void onResponse(String response) {
-//                                Toast.makeText(MainActivity.this,response,Toast.LENGTH_SHORT).show();
-//                            }
-//                        }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(MainActivity.this,"Error occured",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//                // Add the request to the RequestQueue.
-//                queue.add(stringRequest);
+                MySingleton.getInstance(MainActivity.this).addToRequestQueue(request);
 
-               // Toast.makeText(MainActivity.this,"You clicked me 1.",Toast.LENGTH_SHORT).show();
             }
         });
         btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
